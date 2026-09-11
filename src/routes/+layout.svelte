78e8537aaabfccfe77af147cbf86e0f4fa90faea
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-10 21:08:37 -0400

    add bahunya styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/gh/kimeiga/bahunya/dist/bahunya.min.css"
+	/>
 </svelte:head>
 
 {@render children()}
