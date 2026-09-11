2b08479feb73b0608af52cece869a0ab147e8594
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-10 21:09:24 -0400

    add temporary desmos embed
    This is *not* a long-term solution. It's just so that the page isn't
    blank and actually functions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Stardance Mult Utility</h1>
+<p>
+	Tools to visualize and explore the formula that <a
+		href="https://stardance.hackclub.com/">Stardance</a
+	> uses to calculate project multipliers.
+</p>
+
+<section id="desmos">
+	<h2>Interactive Graph</h2>
+	<p>
+		Drag the point side-to-side to adjust the percentile, and it'll display
+		the multiplier.
+	</p>
+	<p>
+		<a href="https://www.desmos.com/calculator/ww5xp3gi52"
+			>Source on Desmos</a
+		> (made by Jonah Crawford)
+	</p>
+	<iframe
+		title="Stardance Multiplier"
+		src="https://www.desmos.com/calculator/jhptzj9o1h?embed"
+		width="500"
+		height="500"
+		frameborder="0"
+	></iframe>
+</section>
+
+<style>
+	#desmos iframe {
+		display: block;
+		margin-inline: auto;
+		border-radius: 1em;
+		background: white;
+	}
+</style>
