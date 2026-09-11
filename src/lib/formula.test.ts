5169d414f570948d933a85206d0a96568e2e64eb
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-11 17:00:44 -0400

    refactor formula tests

--- src/lib/formula.test.ts
+++ src/lib/formula.test.ts
@@ -1,65 +1,68 @@
 import { describe, expect, it } from "vitest";
 import { percentileToMult } from "./formula";
 
+interface Answers {
+	0: number;
+	50: number;
+	100: number;
+}
+
+interface TestCase {
+	name: string;
+	modifier: number;
+	minMult: number;
+	maxMult: number;
+	answers: Answers;
+}
+
+const tests: TestCase[] = [
+	{
+		name: "normal",
+		answers: { 0: 1, 100: 20, 50: 6.67 },
+		modifier: 1,
+		minMult: 1,
+		maxMult: 20,
+	},
+	{
+		name: "blessed",
+		answers: { 0: 1.2, 100: 24, 50: 8 },
+		modifier: 1.2,
+		minMult: 1,
+		maxMult: 20,
+	},
+	{
+		name: "cursed",
+		answers: { 0: 0.5, 100: 10, 50: 3.33 },
+		modifier: 0.5,
+		minMult: 1,
+		maxMult: 20,
+	},
+	{
+		name: "classic",
+		answers: { 0: 0.3, 100: 30, 50: 9.16 },
+		modifier: 1,
+		minMult: 0.3,
+		maxMult: 30,
+	},
+];
+
 describe("Formula", () => {
 	it("should run without errors", () => {
 		expect(() => percentileToMult(50)).not.toThrow;
 	});
 
-	describe("regular", () => {
-		const formula = percentileToMult;
+	describe.each<TestCase>(tests)(
+		"$name",
+		({ name, answers, modifier, minMult, maxMult }) => {
+			const formula = (percentile: number) =>
+				percentileToMult(percentile, modifier, minMult, maxMult);
 
-		it("should be 1x for 0th percentile", () => {
-			expect(formula).toBe(1);
-		});
-		it("should be 20x for 100th percentile", () => {
-			expect(formula).toBe(20);
-		});
-		it("should be ~6.67x for 50th percentile", () => {
-			expect(formula).toBeCloseTo(6.67);
-		});
-	});
-
-	describe("blessed", () => {
-		const formula = (percentile: number) => percentileToMult(percentile, 1.2);
-
-		it("should be 1.2x for 0th percentile", () => {
-			expect(formula(0)).toBeCloseTo(1.2);
-		});
-		it("should be 24x for 100th percentile", () => {
-			expect(formula(100)).toBe(24);
-		});
-		it("should be ~8x for 50th percentile", () => {
-			expect(formula(50)).toBeCloseTo(8);
-		});
-	});
-
-	describe("cursed", () => {
-		const formula = (percentile: number) => percentileToMult(percentile, 0.5);
-
-		it("should be 0.5x for 0th percentile", () => {
-			expect(formula(0)).toBeCloseTo(0.5);
-		});
-		it("should be 10x for 100th percentile", () => {
-			expect(formula(100)).toBe(10);
-		});
-		it("should be ~3.33x for 50th percentile", () => {
-			expect(formula(50)).toBeCloseTo(3.33);
-		});
-	});
-
-	describe("30x max mult", () => {
-		const formula = (percentile: number) =>
-			percentileToMult(percentile, 1, 0.3, 30);
-
-		it("should be 1x for 0th percentile", () => {
-			expect(formula(0)).toBe(0.3);
-		});
-		it("should be 30x for 100th percentile", () => {
-			expect(formula(100)).toBe(30);
-		});
-		it("should be ~9.16x for 50th percentile", () => {
-			expect(formula(50)).toBeCloseTo(9.16);
-		});
-	});
+			it.each(Object.entries(answers))(
+				"%dth percentile should be %dx",
+				(percentile, mult) => {
+					expect(formula(Number(percentile))).toBeCloseTo(mult);
+				},
+			);
+		},
+	);
 });
