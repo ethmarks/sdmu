f2c9eb63442bd929b5882331e88f44f0464d8599
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-23 12:17:35 -0400

    add viral mult range to modifiers

--- src/lib/Modifiers.svelte
+++ src/lib/Modifiers.svelte
@@ -9,12 +9,13 @@
 	let ratingModifier: number = $state(1);
 
 	const MULT_RANGE_MAP = {
-		current: [1, 20] as [number, number],
+		normal: [1, 20] as [number, number],
+		viral: [20, 30] as [number, number],
 		classic: [0.3, 30] as [number, number],
 	} as const;
 	type MultRangeName = keyof typeof MULT_RANGE_MAP;
 
-	let multRangeName: MultRangeName = $state("current");
+	let multRangeName: MultRangeName = $state("normal");
 
 	$effect(() => {
 		const multRange: [number, number] = MULT_RANGE_MAP[multRangeName];
@@ -75,26 +76,47 @@
 <fieldset>
 	<legend>Mult Range</legend>
 
-	<p>Did you know that the maximum mult used to be 30x?</p>
+	<p>
+		<strong>Normal</strong>: the default mult range for the vast majority of
+		projects is 1x-20x
+	</p>
 
 	<p>
+		<strong>Viral</strong>: if your project
+		<a href="https://stardance.hackclub.com/resources/virality_bonus"
+			>goes viral</a
+		>, the mult range becomes 20x-30x
+	</p>
+
+	<p>
+		<strong>Classic</strong>: before
 		<a
 			href="https://hackclub.slack.com/archives/C0AR0M43H61/p1781719856522069"
-			>They changed it a few months ago</a
-		> and they almost certainly aren't going to change it back, but it's interesting
-		to see how things might have been if they didn't change it.
+			>they changed it</a
+		>, the mult range used to be 0.3x-30x
 	</p>
 
 	<div>
 		<input
-			id="current"
+			id="normal"
 			type="radio"
 			name="multRange"
-			value="current"
+			value="normal"
 			checked
 			bind:group={multRangeName}
 		/>
-		<label for="current">Current (1x to 20x)</label>
+		<label for="normal">Normal (1x to 20x)</label>
+	</div>
+
+	<div>
+		<input
+			id="viral"
+			type="radio"
+			name="multRange"
+			value="viral"
+			bind:group={multRangeName}
+		/>
+		<label for="viral">Viral (20x to 30x)</label>
 	</div>
 
 	<div>
@@ -108,8 +130,3 @@
 		<label for="classic">Classic (0.3x to 30x)</label>
 	</div>
 </fieldset>
-
-<p>
-	If your project is viral, you get a flat 30x regardless of how other people
-	rate it, but rating modifiers (blessed and cursed) still apply.
-</p>
